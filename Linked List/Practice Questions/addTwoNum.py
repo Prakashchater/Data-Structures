bf0cf53752ebbdf5fe126f6ade1c4b63c0103dcf
Prakash <prakashchater@gmail.com>
2021-05-21 21:36:28 +0530

    practice

--- Linked List/Practice Questions/addTwoNum.py
+++ Linked List/Practice Questions/addTwoNum.py
@@ -37,6 +37,51 @@ class SinglyLL:
                 n = n.ref
             n.ref = new_node
 
+    # Input:
+    # List1: 5->6->3 // represents number 365
+    # List2: 8->4->2 // represents number 248
+    # Output: Resultant
+    # list: 3->1->6 // represents number 613
+    # Explanation: 365 + 248 = 613
+    ##WHEN THE LINKED LIST IS ALREADY REVERSED
+
+    def addTwo(self, first, second):
+        temp = None
+        curr = None
+        res = None
+        carry = 0
+        s = 0
+
+        while first is not None or second is not None:
+            f_data = 0 if first is None else first.data
+            s_data = 0 if second is None else second.data
+            s = carry + f_data + s_data
+
+            carry = 1 if s >= 10 else 0
+            s = s if s < 10 else s % 10
+
+            temp = Node(s)
+
+            if self.head is None:
+                self.head = temp
+            else:
+                curr.ref = temp
+
+            curr = temp
+
+            if first is not None:
+                first = first.ref
+
+            if second is not None:
+                second = second.ref
+
+        if carry > 0:
+            temp.ref = Node(carry)
+
+
+
+##GFG SOLUTION
+"""
     def reverse(self, head):
         curr = head
         prev = None
@@ -83,6 +128,8 @@ class SinglyLL:
 
         res = self.reverse(res)
         return res
+"""
+
 
 
 
@@ -90,18 +137,19 @@ class SinglyLL:
 if __name__ == '__main__':
     LL1 = SinglyLL()
     LL2 = SinglyLL()
+    LL1.addBegin(3)
     LL1.addBegin(4)
     LL1.addBegin(5)
-    LL1.addBegin(3)
     LL1.printLL()
     print("\n")
-    LL2.addBegin(5)
     LL2.addBegin(4)
+    LL2.addBegin(5)
     LL2.printLL()
     print("\n")
 
-
-
+    res = SinglyLL()
+    res.addTwo(LL1.head, LL2.head)
+    res.printLL()
 
 
 
