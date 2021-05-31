d250b88597654eb337c3ae04ae491e25ac87d30b
Prakash <prakashchater@gmail.com>
2021-05-31 22:26:16 +0530

    Update

--- Stack/stack.py
+++ Stack/stack.py
@@ -18,3 +18,23 @@ class Stack:
     def getStack(self):
         return self.data
 
+class Stack:
+    def __init__(self):
+        self.data = []
+
+    def push(self, item):
+        return self.data.append(item)
+
+    def pop(self):
+        return self.data.pop()
+
+    def isEmpty(self):
+        return self.data == []
+
+    def peek(self):
+        if not self.isEmpty():
+            return self.data[-1]
+
+    def getStack(self):
+        return self.data
+
