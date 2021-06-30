e7a850a3b56d7b76096243dbbefe07501505e6d6
Prakash <prakashchater@gmail.com>
2021-07-01 00:38:24 +0530

    Update

--- Revision.py
+++ Revision.py
@@ -48,19 +48,62 @@
 # print(ob.solve(L))
 
 
-def moveEnd(arr):
-    k = 0
-    if len(arr) == 0:
-        return []
+# def moveEnd(arr):
+#     k = 0
+#     if len(arr) == 0:
+#         return []
+#
+#     for i in range(len(arr)):
+#         if arr[i] != 0:
+#             arr[k] = arr[i]
+#             k += 1
+#     for j in range(k, len(arr)):
+#         arr[j] = 0
+#     return arr
+#
+# if __name__ == '__main__':
+#     arr = [2,0,1,4,0,5,6,4,0,1,7]
+#     print(moveEnd(arr))
 
-    for i in range(len(arr)):
-        if arr[i] != 0:
-            arr[k] = arr[i]
-            k += 1
-    for j in range(k, len(arr)):
-        arr[j] = 0
-    return arr
 
-if __name__ == '__main__':
-    arr = [2,0,1,4,0,5,6,4,0,1,7]
-    print(moveEnd(arr))
+
+def printTwinPrime(n):
+    # Create a boolean array "prime[0..n]"
+    # and initialize all entries it as
+    # true. A value in prime[i] will
+    # finally be false if i is Not a prime,
+    # else true.
+#     prime = [True for i in range(n + 2)]
+#     p = 2
+#
+#     while (p * p <= n + 1):
+#
+#         # If prime[p] is not changed,
+#         # then it is a prime
+#         if (prime[p] == True):
+#
+#             # Update all multiples of p
+#             for i in range(p * 2, n + 2, p):
+#                 prime[i] = False
+#         p += 1
+#
+#     # check twin prime numbers
+#     # display the twin prime numbers
+#     for p in range(2, n - 1):
+#         if prime[p] and prime[p + 2]:
+#             print("(", p, ",", (p + 2), ")", end='')
+#
+#
+# # driver program
+# if __name__ == '__main__':
+#     # static input
+#     n = 25
+#
+#     # Calling the function
+#     printTwinPrime(n)
+
+
+import datetime
+now = datetime.datetime.now()
+current_time = now.strftime("%H:%M:%S")
+print("Current Time =", current_time)
