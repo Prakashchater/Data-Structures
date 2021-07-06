988ba0e4ff26fec3649d3322f396749c04d322a6
Prakash <prakashchater@gmail.com>
2021-07-06 23:04:47 +0530

    Traversal

--- Binary Tree/main.py
+++ Binary Tree/main.py
@@ -63,6 +63,7 @@ tree.root.left.right = Node(5)
 tree.root.right.left = Node(6)
 tree.root.right.right = Node(7)
 tree.root.right.right.left = Node(8)
+tree.root.right.right.right = Node(9)
 
 
 print("PreOrder Traversal ")
